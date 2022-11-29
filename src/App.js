4caa46b4b19429afda449096141c43589bfa33bf
WartHoe <58705434+WartHoe@users.noreply.github.com>
2022-11-29 23:06:06 +0300

    alpha

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className="wrapper clear">
+    <header className="d-flex">
+      <div className="d-flex align-center">
+        <img width={40} height={40} src="/img/list.svg" />
+        <h1>The Brainstorm Project v1.0</h1>
+      </div>
+    </header>
+    <div className="content d-block">
+      <h2>Choose the amount of time here:</h2>
+      <div className="contentTime d-flex justify-center align-center">
+        <input type="number" id="timeH" name="H"/>
+        <img width={10} height={50} src="/img/dots.svg" />
+        <input type="number" id="timeM" name="M"/>
+        <img width={10} height={50} src="/img/dots.svg" />
+        <input type="number" id="timeS" name="S"/>
+      </div>
+      <div className="contentTimeLetters d-flex justify-center align-center">
+        <div>H</div>
+        <span>M</span>
+        <div>S</div>
+      </div>
+
+      <div className="contentStart">
+        <h3>And press this button when you’ll be ready :)</h3>
+        <div className="buttonStart d-flex justify-center align-center"><p>start</p></div>
+      </div>
     </div>
+  </div>
   );
 }
 
